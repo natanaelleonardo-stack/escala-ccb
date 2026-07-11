98f54664a298963750709166fe74f5011e04c082
natanaelleonardo-stack <natanael.leonardo@gmail.com>
2026-07-11 17:28:56 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Os dados (Firestore) NÃO são cacheados aqui — precisam de internet.
 // ════════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'escala-porteiros-v5';
+const CACHE_NAME = 'escala-porteiros-v6';
 
 const ARQUIVOS_CACHE = [
   './',
